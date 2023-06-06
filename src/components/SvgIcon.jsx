edc5d06928d5d249ecd252fe06c3809c268080e9
Nguyen Duc Thai <thaivt1312@gmail.com>
2023-06-06 21:43:36 +0700

    fix result pic

--- src/components/SvgIcon.jsx
+++ src/components/SvgIcon.jsx
@@ -4,8 +4,8 @@ export default function SvgIcon({src, width, height, name, type, alt, className}
     const url = src ? src : `/${name}.${type ? type : 'svg'}`;
 
     return (
-        <div className={className}>
-            <img  src={url} alt={alt} style={{display: 'flex', width, height}}/>
-        </div>
+        // <div >
+        <img className={className} src={url} alt={alt} style={{display: 'flex', width, height}}/>
+        // </div>
     )
 }
