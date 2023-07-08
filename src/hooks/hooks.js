502e996a5ff2243937cb6f14c4ffbacc11f52c69
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2023-07-09 00:50:37 +0700

    no commit

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -4,6 +4,10 @@ const publicURL =
 const apiURL = "https://yap36pcaypqwdyhfs5dku222bi0yeokj.lambda-url.ap-southeast-1.on.aws/" + "api"
 
 const headersWithToken = {
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+      },
     accept: 'application/json',
     Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
     "Content-Type": "multipart/form-data",
