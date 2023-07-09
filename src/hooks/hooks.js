54731ec11a667a234831f680bde6ce0f5d57ed09
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2023-07-09 21:43:30 +0700

    no commit

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -3,12 +3,15 @@ import cors from 'cors';
 const publicURL = 
                 // "http://localhost:8000/"
                 ""
-const apiURL = "https://dry-dawn-29479-ac33aa1a4e62.herokuapp.com/" + "api"
+const apiURL = "https://rraqdwongf72vlviym7iquvfxq0vmnpe.lambda-url.ap-southeast-1.on.aws/" + "api"
 
 const headersWithToken = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Credentials': 'true',
+    headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+        { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+      ],
 
     accept: 'application/json',
     Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
