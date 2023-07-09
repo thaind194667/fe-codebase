e5575bc68749b28f89e35c33c34beb1641258d6f
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2023-07-09 22:18:06 +0700

    no commit

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -6,13 +6,6 @@ const publicURL =
 const apiURL = "https://c69a-2402-800-617f-2239-4848-964d-4132-c473.ngrok-free.app/" + "api"
 
 const headersWithToken = {
-    headers: [
-        { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        { key: 'Access-Control-Allow-Origin', value: '*' },
-        { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-        { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-      ],
-
     accept: 'application/json',
     Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
     "Content-Type": "multipart/form-data",
