f1098e036b4e710663397784f9ca688b45d42de5
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2023-07-09 16:09:48 +0700

    done

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -1,11 +1,15 @@
+import cors from 'cors';
+
 const publicURL = 
                 // "http://localhost:8000/"
                 ""
 const apiURL = "https://dry-dawn-29479-ac33aa1a4e62.herokuapp.com/" + "api"
 
 const headersWithToken = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Credentials': 'true',
+
     accept: 'application/json',
     Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
     "Content-Type": "multipart/form-data",
